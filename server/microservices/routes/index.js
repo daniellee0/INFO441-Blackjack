0d5c23f118a0aa3d99ccde0658bacabf68dc4022
munyii18386 <lmburu@uw.edu>
2019-12-03 19:10:32 -0800

    added unregister route

--- server/microservices/routes/index.js
+++ server/microservices/routes/index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const mysql = require("mysql");
 const app = express();
+
 let connection = mysql.createConnection({
     host     : 'localhost',
     user     : "root",
@@ -9,29 +10,41 @@ let connection = mysql.createConnection({
     database : "blackjack"
   });
 
- 
 
+function validate(req, res){
 
-app.route("/v1/Users/register")
+    return ( req.body.name == undefined  && (req.body.name.length <= 1 || name == null))
+        // res.status(400).send("Bad Request");
+    
+
+}
+
+app.route('/v1/Users/register')
   .post((req, res) =>{
     
-    if( req.body.name == null || req.body.name.length < 1){
-        res.status(400).send("Bad Request")
-    }
+   validate(req, res);
 
     const user = {
         name : req.body.name,
         status: req.body.status,
         chips: req.body.chips,
         cards: req.body.cards.toString()
-  
     }
 
     connection.query('INSERT INTO Users SET ?', user, (e, results) =>{
        res.status(201).send("Player registered")
-    })
+    });
     
-  })
+  });
+
+app.route('/v1/Users/:playerName/unregister')
+    .delete((req, res) =>{
+        let name = req.params.playerName;
+        connection.query('DELETE FROM Users WHERE name = ?', name, (e, r) =>{
+            if(e)res.status(400).send("Bad request");
+            res.status(201).send("Player Unregistered");
+        });
+});
 
 
 
