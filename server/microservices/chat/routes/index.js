c29ba0763b38e23fdec71758fc591ddcd15baad0
donloby <donloby@uw.edu>
2019-12-16 12:06:37 -0800

    Remove comments

--- server/microservices/chat/routes/index.js
+++ server/microservices/chat/routes/index.js
@@ -11,240 +11,12 @@ let connection = mysql.createConnection({
   });
 
 
-  app.route('/v1/channels')
-  .get((req, res) =>{
-      if(!req.get('X-User')) {
-          res.status(401).send("Unauthorized");	
-          return;
-      } 
-      connection.query("SELECT * FROM Channels", (err, results) =>{
-          if (err) throw err;
-          let list = [];
-          let user = JSON.parse(req.get('X-User')).id;
-          for(let i in results){
-              let members =  JSON.parse(results[i].members);
-              for( let x in members){
-                  if (user == members[x]){list.push(results[i].id);}
-              }
-          }
-              analyzeResults(list);    
-          }); 
-          function analyzeResults(list){
-              connection.query(`SELECT * FROM Channels WHERE id IN (${list})`, (err, results) =>{
-                  res.setHeader("Content-Type", "application/json");
-                  res.json(results); 
-              })
-          } 
-  })
- 
-  .post((req, res) =>{
-      if(!req.get('X-User')) {
-          res.status(401).send("Unauthorized");	
-          return;
-      } 
-      let user = JSON.parse(req.get('X-User')).id;
-      let list = {};
-      let creator = req.body.creator;
-      if (creator != null && user != creator) {
-          res.status(401).send("Unauthorized");	
-          return;
-      } else {
-          creator = user
-      }
-      if (req.body.name == null || undefined) {
-          res.status(400).send("Bad request");	
-          return;
-      }
-      let createdAt = req.body.createdAt;
-      let private;
-      if (req.body.private == null || undefined) {
-          private = true;
-      } else {
-          private = req.body.private
-      }
-      list["ID" + user] = user;
-      if(req.body.createdAt == null || undefined){
-          // createdAt = mySQLDate();
-          createdAt = new Date().toISOString().slice(0, 23).replace('T', ' ');
-      }
-      const newChannel = {
-          name: req.body.name,
-          description: req.body.description,
-          private: private,
-          members: JSON.stringify(list),
-          createdAt: createdAt,
-          creator: creator,
-          editedAt: req.body.editedAt
-      };
-      connection.query('INSERT INTO Channels SET ?',newChannel, (err, results) =>{
-          if(err) {
-              res.status(400).send("Bad request");
-              return;
-          };
-          let id = results.insertId;
-          getChannel(id);
-      });
-      function getChannel(id){
-          connection.query('SELECT * FROM Channels WHERE id=?', id, (err, results) =>{
-              res.set("Content-Type", "application/json");
-              res.status(201).json(results[0]);
-              let userIds = [];
-              if (list != null) {
-                  for (var key of Object.keys(list)) {
-                      userIds.push(list[key]);
-                  }
-              } 
-              let data = {
-                  type: 'channel-new',
-                  channel: results[0],
-                  userIDs: userIds
-              };
-              sendToMQ(req, data);
-          });
-      };
-  })
-  .all((req, res) =>{
-      res.set("Content-Type", "text/plain");
-      res.status(405).send("Bad request");
-      return;
-  });
-
   
-app.route('/v1/channels/:channelID/members')
-  .post((req, res) =>{
-      if(req.get('X-User') == null || undefined) return res.status(401).send("Unauthorized");
-      let newMemberID = req.body.id;
-      let user = JSON.parse(req.get('X-User')).id;
-      let id = parseInt(req.params.channelID);
-      connection.query("SELECT * FROM Channels WHERE id=?",id, (err, results)=>{
-          if(err) {
-              res.status(400).send("Bad request");
-              return;
-          };
-          let creator = results[0].creator;
-          if( user != creator){
-              res.set("Content-Type", "text/plain");
-              res.status(403).send("Forbidden");
-              return;
-          } else{
-              updateChannel()
-          }
-      }); 
-      function updateChannel(){
-          connection.query('SELECT * FROM Channels WHERE id=?', id, function (err, results){
-              if(err) {
-                  res.status(400).send("Bad request");
-                  return;
-              };
-              analyzeResults(results[0])   
-          });
-          function analyzeResults(results){
-              let members = JSON.parse(results.members);
-              members["ID" + newMemberID] = newMemberID;
-              let updatedMembers = JSON.stringify(members);
-              connection.query('UPDATE Channels SET members = ? WHERE id = ?', [updatedMembers, results.id], (err, results) =>{
-                  if(err) {
-                      res.status(400).send("Bad request");
-                      return;
-                  };
-                  res.set("Content-Type", "text/plain");
-                  res.status(201).send("member added");
-              })
-          }
-      }
+  
 
-  })
-  .delete((req, res) =>{
-      if(req.get('X-User') == null || undefined) return res.status(401).send("Unauthorized");
-      let user = JSON.parse(req.get('X-User')).id;
-      let id = parseInt(req.params.channelID);
-      connection.query("SELECT * FROM Channels WHERE id=?",id, (err, results)=>{
-          if(err) {
-              res.status(400).send("Bad request");
-              return;
-          };
-          let creator = results[0].creator;
-          if( user != creator){
-              res.set("Content-Type", "text/plain");
-              res.status(403).send("Forbidden");
-          } else{
-              deleteMember()
-          }
-      }); 
-      function deleteMember(){
-          connection.query('SELECT * FROM Channels WHERE id=?', id, function (err, results){
-              if(err) {
-                  res.status(400).send("Bad request");
-                  return;
-              };
-              analyzeResults(results[0])
-          });
-          function analyzeResults(results){
-              let members = JSON.parse(results.members);
-              let rmUser = parseInt(req.body.id);
-             
-              for(let i in members){
-                  if(rmUser == members[i]){
-                      delete members[i];
-                  }
-              }
-              let list = JSON.stringify(members);
-              connection.query('UPDATE Channels SET members = ? WHERE id=?', [list, results.id], (err, results) =>{
-                  if(err) {
-                      res.status(400).send("Bad request");
-                      return;
-                  };
-                  res.set("Content-Type", "text/plain");
-                  res.status(201).send("User deleted");
-              });
-          }
-      }
-  })
-  .all((req, res) =>{
-      res.set("Content-Type", "text/plain");
-      res.status(405).send("Bad request");
-      return;
-  });
 
 app.route('/v1/games/:GameID')
-//   .get((req, res) =>{
-//       if(req.get('X-User') == null || undefined) return res.status(401).send("Unauthorized");
-//       let user = JSON.parse(req.get('X-User')).id;
-//       let id = parseInt(req.params.GameID);
-//       let msgID = req.query.before;
-//       let values;
-//       let query;
-//       if (msgID == null || undefined){
-//           query = "SELECT * FROM Messages WHERE channelID=?";
-//           values = id;
-//       } else {
-//           query = "SELECT * FROM Messages WHERE channelID=? AND id < ? LIMIT 100";
-//           values = [id, msgID];
-//       };
-//       connection.query("SELECT * FROM Channels WHERE id=?",id, (err, results)=>{
-//           if(err) res.status(400).send("Bad request");;
-//           let prv = results[0].private;
-//           let members = JSON.parse(results[0].members)
-//           let valid = 0;
-//           for(let i in members){
-//               if(user == parseInt(members[i])){
-//                  valid = 1;
-//               };
-//           }
-//           if(prv == 1 && valid == 0){
-//               res.status(403).send("Forbidden");
-//           } else{
-//               connection.query(query, values, (err, results) =>{
-//                   if(err) {
-//                       res.status(400).send("Bad request");
-//                       return;
-//                   };
-//                   res.set("Content-Type", "application/json");
-//                   res.json(results);   
-//               });
-//           }
-//       });                   
-//   })
+
   .post((req, res) =>{
         if(req.get('X-User') == null || undefined) return res.status(401).send("Unauthorized");
         let user = JSON.parse(req.get('X-User')).id;
@@ -253,26 +25,7 @@ app.route('/v1/games/:GameID')
             res.status(400).send("Bad request: cannot post blank message");
             return;
         }
-    //   connection.query("SELECT * FROM Channels WHERE id=?",id, (err, results)=>{
-    //       if(err) {
-    //           res.status(400).send("Bad request");
-    //           return;
-    //       };
-    //       let prv = results[0].private;
-    //       let members = JSON.parse(results[0].members);
-    //       let valid = 0;
-    //       for(let i in members){
-    //           if(user == parseInt(members[i])){
-    //              valid = 1;
-    //           };
-    //       }
-    //       if(prv == 1 && valid == 0){
-    //           res.set("Content-Type", "text/plain");
-    //           res.status(403).send("Forbidden");
-    //       } else{
-    //           insertMessage();
-    //       }
-    //   });
+    
         let msg = { 
             player_id: user, 
             body: req.body.body,
@@ -286,15 +39,6 @@ app.route('/v1/games/:GameID')
             let username = results[0].username;
             insertMessage(msg, res, req, username);
         });
-        // connection.query("INSERT INTO Messages SET ?", msg, (err, results) => {
-        //     if(err) {
-        //         res.status(400).send("Bad request");
-        //         return;
-        //     };
-        //     let id = results.insertId;
-        //     addNewMessageResponse(id, res, req, username);
-        // });
-
       
   })
   .patch((req, res) =>{
@@ -528,34 +272,7 @@ function messagePlayers(type, message, req, username) {
     });
 }
 
-// function sendToMsgQueue(channelID, msgID, type, message, req) {
-//   connection.query('SELECT * FROM Messsages WHERE game_id=?', channelID, (err, results) =>{
-      
-//       let members = JSON.parse(results[0].members);
-//       let userIds = [];
-//       if (results[0].private == 1) {
-//           for (var key of Object.keys(members)) {
-//               userIds.push(members[key]);
-//           }
-//       }
-//       let data;
-//       if (type == "delete") {
-//           data = {
-//               type: 'message-delete',
-//               messageID: msgID,
-//               userIDs: userIds
-//           };
-//       } else {
-//           data = {
-//               type: 'message-' + type,
-//               message: message,
-//               userIDs: userIds
-//           };
-//       }
 
-//       sendToMQ(req, data);
-//   });
-// }
 
 function addNewMessageResponse(id, res, req, username){
     connection.query('SELECT * FROM Messages WHERE id=?', id, (err, results) =>{
